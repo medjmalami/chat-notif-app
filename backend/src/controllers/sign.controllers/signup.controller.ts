21d2a6ca2d1ea29a0723a411bcc8c149ef23ad10
amine <jmalmohamedamine1@gmail.com>
2025-09-25 16:52:12 +0100

    sign in endpoint

---
+++ backend/src/controllers/sign.controllers/signup.controller.ts
@@ -0,0 +1,69 @@
+import { Context } from "hono";
+import { z } from "zod";
+import { users, userSessions } from "../../db/schema";
+import { db } from "../../db";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcrypt";
+import  jwt  from "jsonwebtoken";
+
+const reqSchema = z.object({
+    email: z.email(),
+    username: z.string().min(3),
+    password: z.string().min(8),
+});
+interface reqType extends z.infer<typeof reqSchema> {}
+
+export const signupController = async (c : Context) => {
+    try {
+
+        const body = await c.req.json();
+        const validation = reqSchema.safeParse(body);
+        if (!validation.success) {
+            return c.json({messages: "Invalid request"}, 400);
+        }
+
+        const req : reqType = validation.data;
+
+        const manyUsers = await db.select().from(users).where(eq(users.email, req.email)).limit(1);
+
+        if (manyUsers.length) {
+            return c.json({ message: "User already exists" }, 400);
+        }
+
+        const hashedPassword = await bcrypt.hash(req.password, 10);
+
+        const userId = await db.insert(users).values({
+            email: req.email,
+            username: req.username,
+            passwordHash: hashedPassword,
+        }).returning();
+
+
+
+        const user = userId[0];
+        const payload  = {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+        };
+        const accessToken =  jwt.sign(payload,process.env.ACCESS_TOKEN_SECRET!,{ expiresIn: '1h' });
+        const refreshToken =  jwt.sign(payload,process.env.REFRESH_TOKEN_SECRET!,{ expiresIn: '7d' });
+        await db.insert(userSessions).values({
+            userId: user.id,
+            refreshToken,
+            expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+        });
+        //i have to return access token, refresh token , user id , username, chats(id, name, type)
+        return c.json({
+            accessToken,
+            refreshToken,
+            userId: user.id,
+            username: user.username,
+            chats: [],
+        });
+
+
+    } catch (error) {
+        return c.json({ message: "Internal server error" }, 500);
+    }
+};
--- backend/src/routes/signRoutes.ts
+++ backend/src/routes/signRoutes.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono";
+import { signupController } from "../controllers/sign.controllers/signup.controller";
+import { signinController } from "../controllers/sign.controllers/signin.controller";
 
 export const signRoutes = new Hono();
 
-signRoutes.post("/", (c) => {
-  return c.json({ message: "Hello World" });
-});
+signRoutes.post("/signup", signupController);
+signRoutes.post("/signin", signinController);

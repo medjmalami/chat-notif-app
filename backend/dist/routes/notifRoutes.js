e7ebf8305e32941f6b50e2250cd530f51c2bfbc4
amine <jmalmohamedamine1@gmail.com>
2025-10-11 11:06:38 +0100

    added build and start scripts

--- backend/dist/routes/notifRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Hono } from "hono";
-import { getNotifsController } from "../controllers/notifs.controller/getNotifs.controller";
-import { authMiddleware } from "../utils/middleware";
-const notifRoutes = new Hono();
-notifRoutes.get("/notifs", authMiddleware, getNotifsController);
-export default notifRoutes;
